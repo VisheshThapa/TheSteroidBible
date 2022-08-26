702f11359a2712cf7b86bff20abd4e18dccce3d5
Vishesh <visheshjthapa@gmail.com>
2022-08-26 14:06:15 -0700

    Add project files.

---
+++ SteroidBible/Methods/Data.cs
@@ -0,0 +1,13 @@
+﻿namespace SteroidBible.Methods
+{
+    public class Root
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Img { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Source { get; set; } = string.Empty;
+        public List<string>? Weeks { get; set; }  
+        public List<List<List<string>>>? Cycle { get; set; }
+    }
+
+}
